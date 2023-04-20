9bc55e6047313d098c21d6d45a4411be4a6f92a7
Johan Daniel Aguirre Arias <1110290182@u.icesi.edu.co>
2023-04-20 08:00:52 -0500

    make optimizations in the code and create the method binary search

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,7 +8,7 @@ public class Main {
     private static BST tree;
     public static void main(String[] args) {
         Main m = new Main();
-        tree = new BST(null);
+        tree = new BST();
         System.out.println("ingrese los datos con un espacio entre ellos");
         String names = scanner.nextLine();
         String[] array = names.split(" ");
@@ -21,11 +21,18 @@ public class Main {
         String a = "";
         int i = 0;
         alist = tree.sort(alist);
-        tree.setRoot(tree.maketree(alist,0,alist.size()-1));
         do {
             a = a + alist.get(i) + " ";
             i++;
         } while (i != alist.size());
         System.out.println(a);
+        System.out.println("escriba un parametro a buscar");
+        String names = scanner.nextLine();
+        String y=tree.binarysearch(alist,names);
+        if(y.equalsIgnoreCase("-1")){
+            System.out.println("no existe en la lista");
+        }else{
+            System.out.println("el numero se encuentra en la posicion "+ y);
+        }
     }
 }
